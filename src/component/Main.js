679fb3f2db20f3b014e66cfdc7b050d9c2aac78c
Firdous Ali <firdous.ali.tp@gmail.com>
2026-02-20 23:03:30 +0500

    infinite loop fix

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import useFetch from "../hooks/useFetch";
 import Search from "./Search";
 import List from "./List";
@@ -7,29 +7,32 @@ const Main = () => {
   //custom hooks
   const { data, loading, error } = useFetch("https://dummyjson.com/users");
   const [fetchUsers, setFetchUsers] = useState([]);
-  const [userData, setUserData] = useState([])
+  const [userData, setUserData] = useState([]);
 
   useEffect(() => {
     setUserData(data);
     setFetchUsers(data);
   }, [data]);
 
-  const handleSearch = (search) => {
-    //handle case
-    const filterUser = fetchUsers.filter((data) =>
-      `${data.firstName} ${data.lastName}`
-        .toLowerCase()
-        .includes(search.toLowerCase()),
-    );
-    setFetchUsers(filterUser);
-  };
+  const handleSearch = useCallback(
+    (search) => {
+      //handle case
+      const filterUser = userData.filter((user) =>
+        `${user.firstName} ${user.lastName}`
+          .toLowerCase()
+          .includes(search.toLowerCase()),
+      );
+      setFetchUsers(filterUser);
+    },
+    [userData],
+  );
 
   return (
     <div>
       <Search search={handleSearch} />
       {loading && <p>Loading ...</p>}
       {error && <p>{error}</p>}
-      <List users={fetchUsers ? fetchUsers : userData} />
+      <List users={fetchUsers} />
     </div>
   );
 };
